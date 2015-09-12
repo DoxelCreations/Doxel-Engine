b7b264a69610a9002a269f1f60163c38af340f41
Nitzan Elbaz <nitzanel2@gmail.com>
2015-09-12 16:38:24 +0300

    Added shaders
    Added a simple vertex shader and a simple fragment shader.

---
+++ Doxel-Engine Solution/Doxel-Engine/Shaders/Shader.frag
@@ -0,0 +1,10 @@
+#version 330 core
+
+in vec4 outColor; // this is the vec4 we send out of the vertex shader.
+
+out vec4 fragColor; // this is the output color we get out of the fragment shader.
+ 
+void main()
+{
+    fragColor = outColor; // set the fragColor to be outColor.	
+}
--- Doxel-Engine Solution/Doxel-Engine/Window.h
+++ Doxel-Engine Solution/Doxel-Engine/Window.h
@@ -24,21 +24,21 @@ public:
 Window(); ///<  empty.
 ~Window(); ///< empty.
 
-bool init(int width , int height , char* title = DEFAULT_TITLE); // Initlialize the window, will return true if successful. If no title is given at function call, it will use the DEFAULT_TITLE, "Doxel Engine".
-bool isInitialized() { return m_window != nullptr; } // Checks if the GLFWwindow is already initlialized.
+bool init(int width , int height , char* title = DEFAULT_TITLE); ///< Initlialize the window, will return true if successful. If no title is given at function call, it will use the DEFAULT_TITLE, "Doxel Engine".
+bool isInitialized() { return m_window != nullptr; } ///< Checks if the GLFWwindow is already initlialized.
 
 void destroy(); ///< Destroy the window.
 
 void update(); ///< To be called at the end of every frame.
 
 /// Setters
-void setWindowClose(); // Call it when you want to make the window close.
-bool shouldWindowClose(); // Can be used as the boolean for the game loop.
+void setWindowClose();///< Call it when you want to make the window close.
+bool shouldWindowClose(); ///< Can be used as the boolean for the game loop.
 
 /// Getters
-double getTimePerFrame() { return m_currentTimePerFrame; } // Returns avarage time for each frame in the last second.
-double getFramesPerSecond() { return 1000.0 / m_currentTimePerFrame; } // Returns number of frames for the last second.
-GLFWwindow* getWindowHandler() { return m_window; } // Returns a GLFWwindow pointer.
+double getTimePerFrame() { return m_currentTimePerFrame; } ///< Returns avarage time for each frame in the last second.
+double getFramesPerSecond() { return 1000.0 / m_currentTimePerFrame; } ///< Returns number of frames for the last second.
+GLFWwindow* getWindowHandler() { return m_window; } ///< Returns a GLFWwindow pointer.
 
 
 private:
